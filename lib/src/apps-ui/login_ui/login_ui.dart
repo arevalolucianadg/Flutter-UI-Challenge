83c120af34cb9f21b20b570945b527025b66a7b9
Luciana <arevalolucianadg@gmail.com>
2021-03-09 19:32:36 -0300

    Login folder restructuring

--- lib/src/apps-ui/login_ui/login_ui.dart
+++ lib/src/apps-ui/login_ui/login_ui.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'package:flutter_ui_challenge/src/apps-ui/main_page.dart';
+import 'package:flutter_ui_challenge/src/apps-ui/login_ui/main_page.dart';
 
 class LoginUi extends StatelessWidget {
   @override
